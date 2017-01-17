681bbe4cc72c4e8387cc504eda554bd1e699a56c
Heiko Braun <ike.braun@googlemail.com>
2017-01-17 12:33:56 +0100

    initial import

---
+++ src/main/java/org/obsidiantoaster/quickstart/service/DataResource.java
@@ -0,0 +1,33 @@
+package org.obsidiantoaster.quickstart.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * @author Heiko Braun
+ * @since 17/01/2017
+ */
+@Path("/")
+public class DataResource {
+
+    @GET
+    @Produces("text/plain")
+    public String get() throws NamingException, SQLException {
+        Context ctx = new InitialContext();
+        DataSource ds = (DataSource) ctx.lookup("jboss/datasources/ExampleDS");
+        Connection conn = ds.getConnection();
+        try {
+            return "Using datasource driver: " + conn.getMetaData().getDriverName();
+        } finally {
+            conn.close();
+        }
+    }
+}
