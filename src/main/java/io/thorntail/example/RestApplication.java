c3de0639ea11c896e7003ededafd0c5b2a808943
Ladislav Thon <lthon@redhat.com>
2019-03-27 14:31:34 +0100

    [THORN-2357] update to Thorntail 2.4.0.Final

--- src/main/java/io/thorntail/example/RestApplication.java
+++ src/main/java/io/thorntail/example/RestApplication.java
@@ -13,8 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
-package io.openshift.boosters;
+package io.thorntail.example;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
